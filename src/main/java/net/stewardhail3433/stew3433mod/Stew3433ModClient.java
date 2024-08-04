32d855537d7793f39c8c496982a8ece7e35ab56c
Ethan Porter <emanporter2009@gmail.com>
2024-08-03 20:38:34 -0400

    first commit

---
+++ src/main/java/net/stewardhail3433/stew3433mod/Stew3433ModClient.java
@@ -0,0 +1,12 @@
+package net.stewardhail3433.stew3433mod;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class Stew3433ModClient implements ClientModInitializer{
+
+    @Override
+    public void onInitializeClient() {
+       
+    }
+    
+}
---
+++ src/main/java/net/stewardhail3433/stew3433mod/Stew3433ModDataGenerator.java
@@ -0,0 +1,11 @@
+package net.stewardhail3433.stew3433mod;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class Stew3433ModDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
